346406d259b5134ba6f7d3c446aae65abb39bd0f
Remania <remaniacode@gmail.com>
2022-05-31 12:34:40 -0500

    deploying to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ login_manager.login_message_category = "info"
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "samplesecret"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///askify.db"
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///askify.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://saoacykbmyuojp:3f8bc32fb66e3457b9f7be3e68bf452b31f64ba9a57457b559b3a4665f7fac8a@ec2-54-204-56-171.compute-1.amazonaws.com:5432/deanfmhvm4tn1f"
 
     db.init_app(app)
     bcrypt.init_app(app)
--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text(), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     comments = db.relationship('Comment', backref='post',
